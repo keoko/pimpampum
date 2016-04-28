3f1e348447bb67ddd88e05a9787fa975c117f35a
natxo cabre <natxo.cabre@gmail.com>
2016-04-29 00:15:26 +0200

    init duct project

---
+++ project.clj
@@ -0,0 +1,39 @@
+(defproject pimpampum "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.stuartsierra/component "0.3.1"]
+                 [compojure "1.5.0"]
+                 [duct "0.5.10"]
+                 [environ "1.0.2"]
+                 [meta-merge "0.1.1"]
+                 [ring "1.4.0"]
+                 [ring/ring-defaults "0.2.0"]
+                 [ring-jetty-component "0.3.1"]
+                 [duct/hikaricp-component "0.1.0"]
+                 [org.xerial/sqlite-jdbc "3.8.11.2"]
+                 [duct/ragtime-component "0.1.3"]]
+  :plugins [[lein-environ "1.0.2"]
+            [lein-gen "0.2.2"]]
+  :generators [[duct/generators "0.5.10"]]
+  :duct {:ns-prefix pimpampum}
+  :main ^:skip-aot pimpampum.main
+  :target-path "target/%s/"
+  :aliases {"gen"   ["generate"]
+            "setup" ["do" ["generate" "locals"]]}
+  :profiles
+  {:dev  [:project/dev  :profiles/dev]
+   :test [:project/test :profiles/test]
+   :uberjar {:aot :all}
+   :profiles/dev  {}
+   :profiles/test {}
+   :project/dev   {:dependencies [[reloaded.repl "0.2.1"]
+                                  [org.clojure/tools.namespace "0.2.11"]
+                                  [org.clojure/tools.nrepl "0.2.12"]
+                                  [eftest "0.1.1"]
+                                  [kerodon "0.7.0"]]
+                   :source-paths ["dev"]
+                   :repl-options {:init-ns user}
+                   :env {:port "3000"}}
+   :project/test  {}})
---
+++ src/pimpampum/system.clj
@@ -0,0 +1,29 @@
+(ns pimpampum.system
+  (:require [com.stuartsierra.component :as component]
+            [duct.component.endpoint :refer [endpoint-component]]
+            [duct.component.handler :refer [handler-component]]
+            [duct.component.hikaricp :refer [hikaricp]]
+            [duct.component.ragtime :refer [ragtime]]
+            [duct.middleware.not-found :refer [wrap-not-found]]
+            [meta-merge.core :refer [meta-merge]]
+            [ring.component.jetty :refer [jetty-server]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
+
+(def base-config
+  {:app {:middleware [[wrap-not-found :not-found]
+                      [wrap-defaults :defaults]]
+         :not-found  "Resource Not Found"
+         :defaults   (meta-merge api-defaults {})}
+   :ragtime {:resource-path "pimpampum/migrations"}})
+
+(defn new-system [config]
+  (let [config (meta-merge base-config config)]
+    (-> (component/system-map
+         :app  (handler-component (:app config))
+         :http (jetty-server (:http config))
+         :db   (hikaricp (:db config))
+         :ragtime (ragtime (:ragtime config)))
+        (component/system-using
+         {:http [:app]
+          :app  []
+          :ragtime [:db]}))))
