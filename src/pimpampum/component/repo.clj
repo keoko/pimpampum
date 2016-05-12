d5b049ef184fd46c109b976232fd2c6bf57fedb1
natxo cabre <natxo.cabre@gmail.com>
2016-05-12 16:12:32 +0200

    added parameters in item endpoint

--- src/pimpampum/component/repo.clj
+++ src/pimpampum/component/repo.clj
@@ -15,9 +15,9 @@
   (find-item [this id]
     (j/query (:spec db) ["SELECT * FROM item WHERE id = ?" id]))
   (add-item! [this item]
-    (j/insert! (:spec db) :item {:id item}))
-  (change-item! [this item new-item]
-    (j/update! (:spec db) :item {:id new-item} ["id = ?" item]))
+    (j/insert! (:spec db) :item item))
+  (change-item! [this id item]
+    (j/update! (:spec db) :item item ["id = ?" id]))
   (remove-item! [this item]
     (j/delete! (:spec db) :item ["id = ?" item]))
 
--- src/pimpampum/endpoint/item.clj
+++ src/pimpampum/endpoint/item.clj
@@ -5,14 +5,21 @@
 
 
 (defn resp [body]
-  {:headers {"Content-Type" "text/html"}
-   :body body})
+  {:body body})
 
 (defn item-endpoint
   [{repo :repo}]
   (context "/item" []
            (GET "/:id" [id] (resp (r/find-item repo id)))
            (GET "/" [] (resp (r/find-all repo)))
-           (POST "/:id" [id] (resp (r/add-item! repo id)))
-           (PUT "/:id/:new-id" [id new-id] (resp (r/change-item! repo id new-id)))
+           (POST "/" req (let [id (get (:form-params req) "id")
+                               sku (get (:form-params req) "sku")
+                               name (get (:form-params req) "name")
+                               item {:id id :sku sku :name name}]
+                           (resp (r/add-item! repo item))))
+           (PUT "/:id" req (let [id (get (:form-params req) "id")
+                               sku (get (:form-params req) "sku")
+                               name (get (:form-params req) "name")
+                               item {:id id :sku sku :name name}]
+                           (resp (r/change-item! repo id item))))
            (DELETE "/:id" [id] (resp (r/remove-item! repo id)))))
